5e0c20ab32aed41b74986cceb0a1dd3f8cea2ce0
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-12-15 21:29:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,8 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_rooms(room_assignments)
-  attendees.each_with_index do |name, index| index = index + 1
+def assign_rooms(attendees)
+  room_assignments.each_with_index do |name, index| index = index + 1
     puts "Hello, #{name}! You'll be assigned to room #{index}!"
   end
 end
